98ba76c2fb8af48992d444467a7eb2b8dab63eaf
shogo <soleil_campus200@yahoo.co.jp>
2014-10-02 21:29:28 +0900

    ファイル読み込み時にソートする仕様に変更
    できればcntを使わずに実装したい。

--- lab/js/fileSelect.js
+++ lab/js/fileSelect.js
@@ -1,63 +1,58 @@
-// スライドの画像データを読み込む関数
-var arr = new Array();
 function fileSelect(evt) {
+	// スライドの画像データを読み込む関数
+	var arr = new Array();
 	// 選択したファイルがFileObjectとしてfilesに代入される
 	var files = evt.target.files;
-	//console.log(files);
-	// var output = [];
-	var reg=/(.*)(?:\.([^.]+$))/;
+
+	var reg = /(.*)(?:\.([^.]+$))/;
+
+	var cnt = 1;
+
+	// ファイル数分、arrをobjectで初期化
+	for(var i = 0;i < files.length; i++){
+		arr[i] = new Object();
+	}
 
 	for (var i = 0, f; f = files[i]; i++) {
-		// output.push(f.name);
 		// dataURL形式でファイルを読み込む
 		var reader = new FileReader();
-
-		reader.onload = (function(theFile) {
+		// ファイル読み込みに成功すると起こるイベント
+		reader.onload = (function(i,theFile) {
 			return function(e) {
-				// 拡張子を除いたファイル名を取得　1.pngなら1
+				// 拡張子を除いたファイル名を取得 1.pngなら1
 				var filename = theFile.name.match(reg)[1];
-
-				// img要素を作成し属性をセット
+				// img要素を作成し属性セット
 				var img = $('<img>').attr({
 					class:"step",
 					src:e.target.result,
 					title:escape(theFile.name),
 					zIndex:filename
 				});
-				$('#slide').prepend(img);
+
+				// keyをimg要素のzIndex属性、valueをimg要素に設定する
+				// localeCompareは1,2,10,13ではなく001,002,010,013と設定しないと、正しい比較ができない
+				var key = img.attr('zIndex');
+				if(key < 10){
+					arr[i].key = '00' + key;
+				}else if(key >= 10 && key < 100){
+					arr[i].key = '0' + key;
+				}
+				arr[i].value = img;
+
+				// 最後のファイル読み込みが完了したら、arrを降順でソートしてslideに追加する
+				// できればcntを使わず実装したいが、今は方法が思いつかない
+				if(cnt == files.length){
+					arr.sort(function(a, b) {
+						return b.key.localeCompare(a.key);
+					});
+					for(i = 0; i < arr.length; i++){
+						$('#slide').append(arr[i].value);
+					}
+				}
+				cnt++;
 			};
-		})(f);
+		})(i,f);
 		reader.readAsDataURL(f);
 		//evt.preventDefault();
 	}
-	/*
-	//すべてを読み込む前にソートしてしまうのでうまく動作しない
-	reader.onloadend = (function(){
-		var arr = new Array();
-
-		// img要素をすべて取り出しarrにセットする
-		$('#slide img').each(function(i){
-			arr[i] = new Object();
-
-			// img要素のzIndex属性の値をkeyにセット。
-			// localeCompareは1,2,10,13ではなく001,002,010,013と設定しないと、正しい比較ができない
-			var key = $(this).attr('zIndex');
-			if(key < 10){
-				arr[i].key = '00' + key;
-			}else if(key >= 10 && key < 100){
-				arr[i].key = '0' + key;
-			}
-			// img要素をvalueにセット。
-			arr[i].value = $(this);
-		});
-		// "key"プロパティで降順ソートする関数
-		arr.sort(function(a, b) {
-			return b.key.localeCompare(a.key);
-		});
-		// 降順されたimg要素をすべてslideに追加する
-		for(i = 0; i < arr.length; i++){
-			$("#slide").append(arr[i].value);
-		}
-	});
-*/
 }
