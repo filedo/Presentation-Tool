e7d3f1446ffe5445a30e70f641e008b85f13214c
shogo <soleil_campus200@yahoo.co.jp>
2014-10-25 13:27:42 +0900

    ファイル整理、チャット実装、アンケートの集計実装
    スライドの読み込みがファイルパスから読み込む方法に変更
    ユーザ認証については、まだテスト段階

--- lab/app.js
+++ lab/app.js
@@ -1,13 +1,13 @@
 var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app),
-    path = require('path'),
-	fs = require('fs');
+io = require('socket.io').listen(app),
+path = require('path'),
+fs = require('fs');
 app.listen(1337);
 function handler(req, res) {
     // ファイルパスから拡張子を取得
     var extname = path.extname(req.url);
     switch(extname) {
-    case '':
+        case '':
         // 現在のディレクトリにあるindex.htmlを読み込む
         fs.readFile(__dirname +'/index.html',function(err,data){
             // errが出れば500番のインターナルサーバエラーを返して終了
@@ -21,34 +21,43 @@ function handler(req, res) {
             res.end();
         });
         break;
-    case '.css':
+        case '.css':
         fs.readFile(__dirname +'/node_modules'+req.url, 'UTF-8',
-            function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/css'});
-                res.write(data);
-                res.end();
-            }
-        );
+                    function (err, data) {
+                        res.writeHead(200, {'Content-Type': 'text/css'});
+                        res.write(data);
+                        res.end();
+                    }
+                    );
         break;
-    case '.js':
+        case '.js':
         fs.readFile(__dirname +'/node_modules'+req.url, 'UTF-8',
-            function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
-                res.write(data);
-                res.end();
-            }
-        );
+                    function (err, data) {
+                        res.writeHead(200, {'Content-Type': 'text/javascript'});
+                        res.write(data);
+                        res.end();
+                    }
+                    );
         break;
-    case '.png':
+        case '.png':
         fs.readFile(__dirname +'/node_modules'+req.url,
-            function (err, data) {
-                res.writeHead(200, {'Content-Type': 'image/png'});
-                res.write(data);
-                res.end();
-            }
-        );
+                    function (err, data) {
+                        res.writeHead(200, {'Content-Type': 'image/png'});
+                        res.write(data);
+                        res.end();
+                    }
+                    );
         break;
-    default:
+        case '.jpg':
+        fs.readFile(__dirname +'/node_modules'+req.url,
+                    function (err, data) {
+                        res.writeHead(200, {'Content-Type': 'image/jpg'});
+                        res.write(data);
+                        res.end();
+                    }
+                    );
+        break;
+        default:
         break;
     }
 }
@@ -62,16 +71,17 @@ io.sockets.on('connection',function(socket) {
 		// socket.broadcast.emit('emit_from_server','hello from server:'+data);
 		// 接続しているソケット全部
 		io.sockets.emit('emit_from_server','['+ socket.id +']：'+data);
+        //io.sockets.emit('emit_from_server',data);
 		// 以下のset、getは現在のsocket.ioのバージョンでは対応しておらず使えない
 		// socket.set('client_name',data.name);
 		// socket.get('client_name',function(err,name){
 		//	io.sockets.emit('emit_from_server','['+ name +']：'+data.msg);
 		//});
 
-	});
-    // jsonObjの情報を現在のディレクトリにtest.txtとして保存する
+    });
+    // jsonObjの情報を現在のディレクトリにjson.txtとして保存する
     socket.on('save_from_client',function(data){
-        var file = 'test.txt';
+        var file = 'json.txt';
         fs.writeFile(file, JSON.stringify(data));
         fs.readFile(file, 'utf8', function (err, data) {
             var a = JSON.parse(data);
@@ -80,4 +90,70 @@ io.sockets.on('connection',function(socket) {
             //console.log(a["comments"][1]["id"]);
         });
     });
+    // question.txtファイルを読み込み、カンマで区切ったリストに変換し、そのリストをすべてのクライアントに送る
+    // answerList:['1','2','4','1'...]
+    socket.on('count_from_client',function(){
+        var file = 'question.txt';
+        fs.readFile(file,'utf8',function(err,data){
+            if (!err){
+                var answerList = data.split(',');
+                // question.txtの最後の','を削除
+                answerList.pop();
+                io.sockets.emit('count_from_server',answerList);
+            }
+        });
+    });
+    socket.on('countDialog_from_client',function(){
+        io.sockets.emit('countDialog_from_server');
+    });
+    // コメントフォームの送信内容を現在のディレクトリにcomment.txtとして保存する
+    socket.on('comment_from_client',function(data1,data2){
+        var commentData = data1 + ": " + data2 + "\n";
+        fs.appendFile('comment.txt',commentData, function (err) {
+            if (err) throw err;
+        });
+    });
+    // アンケートフォームの送信内容を現在のディレクトリにcomment.txtとして保存する
+    socket.on('question_from_client',function(data){
+        var questionData = data + ",";
+        fs.appendFile('question.txt',questionData, function (err) {
+            if (err) throw err;
+        });
+    });
+    socket.on('rmQuestionFile_from_client',function(){
+        fs.unlink(__dirname+'/question.txt',function(err){
+            if (err) throw err;
+        });
+    });
+    socket.on('register_from_client',function(){
+        var file = 'cookie.txt';
+        fs.writeFile(file, socket.id);
+        socket.emit('register_from_server',socket.id);
+    });
+    socket.on('cookie_client',function(id){
+        var file = 'cookie.txt';
+        fs.readFile(file, 'utf8', function (err, data) {
+            if(id==data){
+                socket.emit('presenter_from_server');
+            }
+        });
+    });
+    // 全クライアントに→キーが押されたことを伝える
+    socket.on('nextpage_from_client',function(){
+       io.sockets.emit('nextpage_from_server');
+    });
+    // 全クライアントに←キーが押されたことを伝える
+    socket.on('prepage_from_client',function(){
+       io.sockets.emit('prepage_from_server');
+    });
+
+    // 全クライアントにスライドに使う画像データのファイル数を伝える
+    socket.on('slideload_from_client',function(){
+        var dir = __dirname + '/node_modules/img/';
+        fs.readdir(dir, function(err, files){
+            if(err) throw err;
+           io.sockets.emit('slideload_from_server',files.length);
+        });
+    });
+
 });
